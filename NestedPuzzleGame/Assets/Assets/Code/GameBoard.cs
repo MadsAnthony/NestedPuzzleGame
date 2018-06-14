9c519c542de5f8a63a6e36a1f01b4e65d1baae31
MadsAnthony <mail@madsanthony.com>
2018-06-14 21:50:00 +0200

    Setup a prototype puzzle with Mona Lisa.

---
+++ NestedPuzzleGame/Assets/Assets/Code/GameBoard.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBoard : MonoBehaviour {
+
+	[SerializeField] private GameObject piece;
+
+	public Piece draggablePiece;
+
+	void Start () {
+		Application.targetFrameRate = 60;
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < 3; j++) {
+			var pieceObject = GameObject.Instantiate (piece).GetComponent<Piece>();
+			pieceObject.transform.position = new Vector3 (i,j,0);
+			pieceObject.gameBoard = this;
+				pieceObject.GetComponent<MeshRenderer>().material.SetTextureOffset("_MainTex", new Vector2(i*0.5f,j*0.33f));
+			}
+		}
+	}
+
+	void Update() {
+		if (draggablePiece != null) {
+			var mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			draggablePiece.transform.position = new Vector3(mousePos.x,mousePos.y,draggablePiece.transform.position.z);
+		}
+
+		if (Input.GetMouseButtonUp (0)) {
+			draggablePiece = null;
+		}
+	}
+}
