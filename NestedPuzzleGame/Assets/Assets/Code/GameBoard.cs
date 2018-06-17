8bfeceb89269ceab21e5e3a70d73e6129ff5073a
MadsAnthony <mail@madsanthony.com>
2018-06-17 15:00:06 +0200

    Pieces now snap to grid + work in progress on nested puzzle.

--- NestedPuzzleGame/Assets/Assets/Code/GameBoard.cs
+++ NestedPuzzleGame/Assets/Assets/Code/GameBoard.cs
@@ -5,29 +5,62 @@ using UnityEngine;
 public class GameBoard : MonoBehaviour {
 
 	[SerializeField] private GameObject piece;
+	[SerializeField] private Camera puzzleCamera;
+	[SerializeField] private RenderTexture puzzleCameraTexture;
+	[SerializeField] private Texture snapShot;
 
 	public Piece draggablePiece;
+	public Vector3 draggablePieceOffset;
 
+	private List<Piece> pieces  = new List<Piece>();
+	private List<Vector3> snapablePoints = new List<Vector3>();
+	private Vector3 startOffset = new Vector3(-1,0,0);
+	private RenderTexture puzzleTexture;
 	void Start () {
 		Application.targetFrameRate = 60;
+
 		for (int i = 0; i < 2; i++) {
 			for (int j = 0; j < 3; j++) {
-			var pieceObject = GameObject.Instantiate (piece).GetComponent<Piece>();
-			pieceObject.transform.position = new Vector3 (i,j,0);
-			pieceObject.gameBoard = this;
+				var newSnapablePoint = new Vector3 (i * 2, j * 2, 0) + startOffset;
+				snapablePoints.Add(newSnapablePoint);
+				var pieceObject = GameObject.Instantiate (piece).GetComponent<Piece>();
+				pieceObject.transform.position = new Vector3 (i+0.2f,j*2+0.2f,0)+startOffset;
+				pieceObject.gameBoard = this;
 				pieceObject.GetComponent<MeshRenderer>().material.SetTextureOffset("_MainTex", new Vector2(i*0.5f,j*0.33f));
+				pieces.Add (pieceObject);
 			}
 		}
 	}
 
+	int counter = 0;
 	void Update() {
-		if (draggablePiece != null) {
-			var mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-			draggablePiece.transform.position = new Vector3(mousePos.x,mousePos.y,draggablePiece.transform.position.z);
+		counter++;
+		if (counter == 10) {
+			Graphics.CopyTexture (puzzleCameraTexture, snapShot);
+			foreach (var piece in pieces) {
+				piece.GetComponent<MeshRenderer> ().material.SetTexture ("_MainTex", snapShot);
+			}
 		}
 
-		if (Input.GetMouseButtonUp (0)) {
+		if (draggablePiece != null) {
+			var mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			draggablePiece.transform.position = new Vector3(mousePos.x,mousePos.y,draggablePiece.transform.position.z)+draggablePieceOffset;
+		}
+
+		if (Input.GetMouseButtonUp (0) && (draggablePiece != null)) {
+			foreach (var snapablePoint in snapablePoints) {
+				if (IsWithinRadius (snapablePoint, draggablePiece.transform.position, 0.2f)) {
+					draggablePiece.transform.position = snapablePoint;
+					break;
+				}
+			}
+
 			draggablePiece = null;
 		}
+	}
+
+	private bool IsWithinRadius(Vector3 pointA, Vector3 pointB, float radius) {
+		var dist = pointA - pointB;
+		return dist.magnitude < radius;
 	}
 }
--- NestedPuzzleGame/Assets/Assets/Code/Piece.cs
+++ NestedPuzzleGame/Assets/Assets/Code/Piece.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class Piece : MonoBehaviour {
 	public GameBoard gameBoard;
 	void OnMouseDown() {
+		var mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		var offSet = transform.position - mousePos;
+		offSet.z = 0;
+		gameBoard.draggablePieceOffset = offSet;
 		gameBoard.draggablePiece = this;
 	}
-
 }
