0bf047db24df2e962e919a1dafad9d2b509e2a1e
MadsAnthony <mail@madsanthony.com>
2018-08-02 22:28:13 +0200

    Setup a preProcess method used by unity cloud build.

--- NestedPuzzleGame/Assets/Code/TitleView.cs
+++ NestedPuzzleGame/Assets/Code/TitleView.cs
@@ -2,9 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
+using MiniJSON;
+
 
 public class TitleView : MonoBehaviour {
+	[SerializeField] private Text versionLeft;
+	[SerializeField] private Text versionRight;
 	public void EnterGame() {
 		Director.TransitionManager.PlayTransition (() => { SceneManager.LoadScene ("WorldSelectScene");}, 0.2f, Director.TransitionManager.FadeToBlack(),  Director.TransitionManager.FadeOut());
+	}
+
+	void Start() {
+		var manifest = (TextAsset) Resources.Load("UnityCloudBuildManifest.json");
+		if (manifest != null)
+		{
+			var manifestDict = Json.Deserialize(manifest.text) as Dictionary<string,object>;
+			versionRight.text = manifestDict ["buildNumber"].ToString ();
+		}
 	}
 }
---
+++ NestedPuzzleGame/Assets/Editor/UnityCloudPrePost.cs
@@ -0,0 +1,18 @@
+﻿#if UNITY_CLOUD_BUILD
+using UnityEngine;
+using System.Collections.Generic;
+using MiniJSON;
+using UnityEditor;
+
+public class UnityCloudPreProcess {
+	public static void PreExport() {
+		var manifest = (TextAsset) Resources.Load("UnityCloudBuildManifest.json");
+		if (manifest != null) {
+			var manifestDict = Json.Deserialize(manifest.text) as Dictionary<string,object>;
+			PlayerSettings.SetApplicationIdentifier (BuildTargetGroup.Android, manifestDict ["buildNumber"].ToString ());
+			PlayerSettings.SetApplicationIdentifier (BuildTargetGroup.iOS, manifestDict ["buildNumber"].ToString ());
+		}
+	}
+
+}
+#endif
